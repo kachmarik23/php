be8ba35ec1327f4a59532dd5120694c636c5e3a7
Oleg Khizchniak <fontaneria1@gmail.com>
2019-06-15 13:28:24 +0200

    проба 1

---
+++ html.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <style>
+    a:link{ color: green}
+    a:visited{color: pink}
+    a:hover{color: red}
+    a:active{color: yellow}
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333333;
+
+    }
+    li{
+      float: left;
+    }
+    li a{
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 16px;
+      text-decoration: none;
+    }
+    li a:hover{
+      background-color: #111111;
+    }
+    table#tab01{
+      width: 100%;
+      background-color: #cccccc;
+          }
+    table#tab01 tr:nth-child(even){/* не четные строки таблы*/
+      background-color: #eee;
+    }
+    table#tab01 tr:nth-child(odd){/* четный строки таблицы*/
+      background-color: #fff;
+    }
+  </style>
+</head>
+<body>
+<div class="content">
+  <h1></h1>
+  <form action="">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+  </form>
+  <!-- ctr+/ -->
+  <del>удаленный текст</del><br>
+  <strong>Важный текст</strong><br>
+  <mark>маркер</mark><br>
+  <ins>подчеркнутый текст</ins><br>
+<sub>нижний регистр</sub> и <sup>верхний регистр</sup>
+<p>&copy</p>
+<pre>Тег pre не убирает лишние пробелы <h1> Типа     не     работает</h1></pre>
+<a href="http://w3schools.com" target="_blank">w3schools.com</a><br>
+<a href="#tips">Переехать на #tips</a>
+
+<ul class="">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Abaut</a></li>
+  <li><a href="#">Contact</a></li>
+</ul>
+
+<table id="tab01" style="margin-top:20px">
+  <tr>
+    <td>1</td>
+    <td>1</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td colspan="2">2</td> <!-- colspan="2" объединить 2 ячейки по ширине -->
+    <td rowspan="2">2</td> <!-- rowspan="2" объединить 2 ячейки по высоте -->
+
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>3</td>
+
+  </tr>
+</table>
+
+<h2 id="tips"  style="margin:600px 0 600px">Вот индинтификатор #tips</h2>
+
+
+
+
+
+</div>
+</body>
+</html>
---
+++ php02.php
@@ -0,0 +1,115 @@
+<?php
+$code = "hello world";
+echo $code.PHP_EOL;
+$code = "Hello";
+echo $code.PHP_EOL;
+$price = 'cart';
+$cart = 'test';
+$$price = 'qwerty';
+echo $cart;
+echo '<br>';
+echo "передача переменной по ссылке, \$x = & \$y".'<br>';
+$y = 'переменная У'.PHP_EOL.'<br/>';
+$x = & $y;
+echo $x;
+$x = 'уже не ровно У'.PHP_EOL.'<br/>';
+echo $y;
+print_r(['1','2','3']);
+echo "<br>";
+define('NAZVANIE_CONSTANTI', 'значение константы');
+$name='NAZVANIE_CONSTANTI';
+echo constant($name).'<br>';
+
+echo NAZVANIE_CONSTANTI.'<br>';// константа без доллара
+echo 'Волшебные константы: __LINE__, __FILE__...их всего 8 - '.PHP_EOL.__LINE__.PHP_EOL.__FILE__.'<br>';
+
+$varOne = 2;
+
+$varOne += 10; // прибавляем к переменной то что справа, так же * и /
+echo '" $varOne += 10"'.' = '.$varOne.'<br>';
+
+$varOne = 20;
+echo '" $varOne /= 2"'.' = '.$varOne /=2;
+
+$integerVar = 123;
+echo '<br>'.'приобразование переменных - (bool) $integerVar'.' = '.(bool)$integerVar;
+echo '<br>'.'приобразование переменных - (string) $integerVar'.' = '.(string)$integerVar;
+echo '<br>'.'приобразование переменных - (integet) $integerVar'.' = '.(int)$integerVar;
+
+$var = 123.123;
+echo '<br>'.'(intenger) $var, если вар = 123,123 будет равно - '. (int)$var;
+echo '<br>'.'епереводим integer во flaot (float)$var - '. (float) $var;
+$string = '123qwerty';
+echo '<br>$string = \'123qwerty\'; = '. (int)$string.PHP_EOL.(float)$string;
+$string1 = 'qwerty123';
+echo '<br>$string1 = \'qwerty123\'; = '. (int)$string1;
+unset($string1);
+echo '<br>'.$string1;
+$arrVar = array ('пременная 1', 'переменная 2', 'переменная 3');// устаревшая конструкция
+$arrVar = ['keyOne'=>'пременная 1', 'переменная 2', 'переменная 3'];
+echo '<br>';
+var_dump($arrVar);
+echo '<br>';
+unset ($arrVar[1]);
+print_r($arrVar);
+$i = 1;
+echo '<br>'.++ $i;
+
+echo '<br>'.'Остаток от деления 203 % 2 = '. 203%2;
+echo '<br><br><br>'.'функция isset для проверки наличия переменной или массива'.'<br>';
+$varTvo = 123;
+if (isset($varTvo)){
+echo $varPrice = $varTvo-12;
+}
+
+$a = "test";
+$b = "anothertest";
+echo '<br>';
+var_dump(isset($a)).PHP_EOL;     // TRUE
+var_dump(isset($a, $b));
+$x = null;
+if (!empty($x)){
+ echo '<br>correcto';
+}
+echo '<br> если 3<5'.PHP_EOL;
+$a = 3;
+$b = 5;
+$a<$b?$varVar='условие верно':$varVar='условие не верно';
+echo $varVar.'<br>';
+echo '<br> если 5<3'.PHP_EOL;
+$varVar1=$b<$a?'условие верно':'условие не верно';;
+echo $varVar1.'<br>';
+echo '<br>'.$price = (25<30)? 'OK':'DUE';
+echo '<br>'.$x=(isset($cart))??(isset($price))??(isset($a));
+
+
+$i = 1;
+switch ($i){
+    case 'значение 1':
+        echo '<br> $i == значение 1';
+        break;
+    case 1:
+        echo '<br> $i == 1';
+        break;
+}
+
+echo '<br><br>';
+echo 'Rounding modes with 9.5' . PHP_EOL;
+var_dump(round(9.5, 0, PHP_ROUND_HALF_UP));
+var_dump(round(9.5, 0, PHP_ROUND_HALF_DOWN));
+var_dump(round(9.5, 0, PHP_ROUND_HALF_EVEN));
+var_dump(round(9.5, 0, PHP_ROUND_HALF_ODD));
+echo'<br>';
+echo 'Rounding modes with 8.5' . PHP_EOL;
+var_dump(round(8.5, 0, PHP_ROUND_HALF_UP));
+var_dump(round(8.5, 0, PHP_ROUND_HALF_DOWN));
+var_dump(round(8.5, 0, PHP_ROUND_HALF_EVEN));
+var_dump(round(8.5, 0, PHP_ROUND_HALF_ODD));
+echo '<br>';
+echo 'Rounding 9.5 = ' . PHP_EOL;
+echo round(9.5).';';
+echo '<br>';
+echo 'floor 9.5 = ' . PHP_EOL;
+echo floor(9.5).';';
+
+?>
