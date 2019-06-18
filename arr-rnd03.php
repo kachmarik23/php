911eea89cae030839b1590cf3d8b2180732bf34a
Oleg Khizchniak <icapt_23@yahoo.com>
2019-06-18 21:09:59 +0200

    tabla&?

--- arr-rnd03.php
+++ arr-rnd03.php
@@ -5,5 +5,25 @@ for ($i=0;$i<ARR_LEN;$i++){
 
     $arr[]=rand(11,100);
 }
-print_r($arr);
+//print_r($arr);
+$html='
+<table border="1">
+    <thead>
+        <th>ID</th>
+        <th>VALUE</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>0</td>
+            <td>VALUE 0</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>VALUE 1</td>
+        </tr>
+        
+    </tbody>
+</table>
+';
+echo $html;
 ?>
