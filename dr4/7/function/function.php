b8e0052a3dd600c31f8d040d142c4cc70fa1f0c1
Oleg Khizchniak <icapt_23@yahoo.com>
2019-07-28 23:48:35 +0200

    гостевая книга

---
+++ dr4/7/function/function.php
@@ -0,0 +1,40 @@
+<?php
+const DIR = 'storage/';
+const FNAME = 'readme.txt';
+function viewForm()
+{
+    $html = '<form action="" method="post">
+<input type="text" name="name" placeholder="Введите имя"><br>
+<textarea name="text" style="width: 520px" placeholder="Введите текст"></textarea><br>
+
+<input type="submit" style="width: 520px" value="Запостить отзыв">
+</form>';
+    return $html;
+}
+
+function fileOpen($dir, $fname)
+{
+    if (!file_exists($dir . $fname)) {
+        echo '<div style="background-color: #cccccc; width: 500px; margin-bottom: 10px; padding: 10px">
+Оставьте отзыв первым!</div>';
+    } else {
+        $file = fopen($dir . $fname, 'r');
+        $fileOpen = fread($file, filesize($dir . $fname));
+        fclose($file);
+        echo '<div style="width: 500px; background-color: antiquewhite; margin-bottom: 10px; 
+padding: 5px; padding-top: 15px">' . $fileOpen . '</div>';
+    }
+}
+
+function fileWrite($dir, $fname)
+{
+    $text = $_REQUEST['text'];
+    $name = $_REQUEST['name'];
+    if (!empty($text)) {
+        $file = fopen($dir . $fname, 'a');
+        fwrite($file, '<b>' . $name . '</b><br>' . $text . '<br><br>');
+        fclose($file);
+        header('Location: index.php');
+    }
+
+}
--- dr4/7/index.php
+++ dr4/7/index.php
@@ -1,1 +1,6 @@
 <?php
+require 'function/function.php';
+fileWrite (DIR,FNAME);
+fileOpen(DIR,FNAME);
+echo viewForm();
+
