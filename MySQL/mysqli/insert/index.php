642ad0b4ce5f0d40e4fe01cb11b105dd5fbaf4bb
Oleg Khizchniak <icapt_23@yahoo.com>
2019-09-04 11:01:30 +0200

    Проверка подключения MySQL вывод ошибок

--- MySQL/mysqli/insert/index.php
+++ MySQL/mysqli/insert/index.php
@@ -5,9 +5,16 @@ const DB_HOST = '127.0.0.1';
 const DB_PASS = '';
 
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-echo $db->connect_error;
+if ($db->connect_errno) {
+    die("Не удалось подключиться к MySQL: " . $db->connect_error) ;
+}
+//echo $db->connect_error; // Ошибки при работе с ДБ
 $data=[];
 $res = $db->query(" INSERT INTO `categories` SET `name`='Категория ".rand(1,100)."'");
+if (!$res){
+    die ("Не удалось вставить строку: (" . $db->errno . ") " . $db->error);
+}
+
 $new_id=$db->insert_id;// выясняем новое id
 echo  $new_id;
 $res = $db->query("SELECT * FROM `categories` WHERE `id`= $db->insert_id LIMIT 1");
