b1c3ca076124d19c3398aa825620e7cfe40ab59c
Oleg Khizchniak <icapt_23@yahoo.com>
2019-10-26 12:36:57 +0200

    исключения

---
+++ throw/index.php
@@ -0,0 +1,17 @@
+<?php
+function inverse($x) {
+    if (!$x) {
+        throw new Exception('Деление на ноль.');
+    }
+    return 1/$x;
+}
+
+try {
+    echo inverse(5) . "\n";
+    echo inverse(0) . "\n";
+} catch (Exception $e) {
+    echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+}
+
+// Продолжение выполнения
+echo "Привет, мир\n";
